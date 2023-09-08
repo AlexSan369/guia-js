c2fb8fa9985a25d6f3bc51964f9d95211c3e50e2
AlexSan369 <trigramasolar@gmail.com>
2023-09-08 18:53:45 -0300

    up

---
+++ variaveis.js
@@ -0,0 +1,38 @@
+// Variáveis
+let volatil = 'Alex';   //Não começar o nome da variável com números
+var variavelGlobal = "San"; 
+const definitivo = 369; //Jamais: let 01definitivo;
+
+console.log(volatil);
+
+volatil = 'TLD';
+console.log(volatil, variavelGlobal, definitivo);
+
+// Multiplas variáveis
+let a = 3,b = 6,c = 9, d = '|o/';
+console.log(a, b, c, d);
+console.log(typeof a, typeof d);
+
+const _iniciandoAssim = 'ok', $assimTambem = 'ok';
+console.log(_iniciandoAssim, $assimTambem);
+//const 1assimNao = 'JavaScript gera erro ao inicializar';
+const comNumeros01 = 'ok';
+const com1numero = 'ok';
+console.log(comNumeros01, com1numero);  
+
+// Case sensitive
+let nomecorrido = 'três';
+let nomeCorrido = 'seis';
+
+console.log(nomecorrido);
+console.log(nomeCorrido);
+
+//    _____Nomes reservados - uso não recomendado___________
+//   | break   | continue | catch  | class | const |  case  | 
+//   | default | debugger | delete | with  | else  | export |
+//   | extends | finally  | for    |   function    |   if   | 
+//   | import  |   new    | return | super | this  |   try  |
+//   | throw   | typeof   | switch | var   | void  |  while |
+//   | yield   |    do    |   in   |  instanceof   |        |
+//    ---3------6---------9------------------:v--- ------ --- *3
+
